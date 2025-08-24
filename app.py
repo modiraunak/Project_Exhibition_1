3233ed27d1ecf38c07bb05d0c8250cee750e7150
Raunak Kumar Modi <raunakk046@gmail.com>
2025-08-24 17:31:00 +0530

    edited

--- app.py
+++ app.py
@@ -9,10 +9,14 @@ from streamlit_webrtc import webrtc_streamer , WebRtcMode , RTCConfiguration
 import av 
 import mediapipe as mp
 import cv2
-
+from ggts import gTTS
 ####################################################### UI #####################################################################################
 
 st.set_page_config(page_title="SpeakSmart + CalmCoreAI ( Real-Time Coach for Public Speaking and Interviews )",page_icon="🗣️",layout= "wide" )
 st.title("SpeakSmart + CalmCoreAI ( Real-Time Coach for Public Speaking and Interviews )")
 st.caption("Pitch Tracking + Confidence Analyzer + Webcam BodyLanguage Overlay")
+
+#################################################### WebRtc Config ####################################################################################
+RTCConfiguration = RTCConfiguration({"iceServers":[{"urls":["stun:stun.l.google.com:19302"]}]})
+
 
